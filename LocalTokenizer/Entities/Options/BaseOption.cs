84957f1a149fa0d66d8734e9acf8014542345c6b
Pedro Felipe Ferreira Lima <felipe.lima1996@hotmail.com>
2023-03-08 15:26:21 -0300

    improv: implemented a template check

--- LocalTokenizer/Entities/Options/BaseOption.cs
+++ LocalTokenizer/Entities/Options/BaseOption.cs
@@ -1,29 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.CommandLine;
+﻿using System.CommandLine;
 using System.CommandLine.Parsing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.CommandLine.Help.HelpBuilder;
 
-namespace LocalTokenizer.Entities.Options
+namespace LocalTokenizer.Entities.Options;
+
+public class BaseOption<T> : Option<T>
 {
-    public class BaseOption<T> : Option<T>
+    public BaseOption(string name, string description, ParseArgument<T> parseArgument, string alias, bool isRequired) : base(name, parseArgument)
     {
-        public BaseOption(string name, string description, ParseArgument<T> parseArgument, string alias, bool isRequired) : base(name, parseArgument)
-        {
-            this.Description = description;
-            this.AddAlias(alias);
-            this.IsRequired = isRequired;
-        }
+        this.Description = description;
+        this.AddAlias(alias);
+        this.IsRequired = isRequired;
+    }
 
-        public BaseOption(string name, string description, string alias, bool isRequired, string[] validValues, T defaultValue) : base(name, description)
-        {
-            AddAlias(alias);
-            IsRequired = isRequired;
-            this.FromAmong(validValues);
-            SetDefaultValue(defaultValue);
-        }
+    public BaseOption(string name, string description, string alias, bool isRequired, string[] validValues, T defaultValue) : base(name, description)
+    {
+        AddAlias(alias);
+        IsRequired = isRequired;
+        this.FromAmong(validValues);
+        SetDefaultValue(defaultValue);
     }
 }
--- LocalTokenizer/Entities/Options/TemplateFileOption.cs
+++ LocalTokenizer/Entities/Options/TemplateFileOption.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using LocalTokenizer.Constants;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using System;
 using System.CommandLine.Parsing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LocalTokenizer.Entities.Options
 {
@@ -29,14 +29,27 @@ namespace LocalTokenizer.Entities.Options
             //Checks if exists
             if (!File.Exists(filePath))
             {
-                result.ErrorMessage = "Template File does not exist";
+                result.ErrorMessage = "Template File does not exist.";
                 return null;
             }
+
             var fileInfo = new FileInfo(filePath);
+
             // Checks valid extension (.json)
             if(fileInfo.Extension != ".json")
             {
-                result.ErrorMessage = "Template File extension not supported. Only JSON files supported";
+                result.ErrorMessage = "Template File extension not supported. Only JSON files are supported.";
+                return null;
+            }
+
+            // Checks if is a valid template
+            JSchema schema = JSchema.Parse(TemplateFileConstants.schemaJson);
+            string fileText = File.ReadAllText(filePath);
+            JObject templateFile = JObject.Parse(fileText);
+
+            if (!templateFile.IsValid(schema))
+            {
+                result.ErrorMessage = "This file is not compatible with a valid template.";
                 return null;
             }
 
